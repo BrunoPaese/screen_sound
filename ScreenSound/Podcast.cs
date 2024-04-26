1e21e79cde0f4e975f6314694504f4cc1b07abe4
Bruno Paese <brunoviniciuspaese@gmail.com>
2024-04-25 21:01:09 -0300

    first commit

---
+++ ScreenSound/Podcast.cs
@@ -0,0 +1,27 @@
+﻿class Podcast
+{
+    public List<Episodio> episodios = new();
+    public Podcast(string host, string nome)
+    {
+        Host = host;
+        Nome = nome;
+    }
+    public string Host { get; }
+    public string Nome { get; }
+    public int TotalEpisodios => episodios.Count;
+
+    public void AdicionarEpisodio(Episodio episodio)
+    {
+        episodios.Add(episodio);
+    }
+
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine($"Podecast {Nome} apresentado por {Host}\n");
+        foreach (var episodio in episodios.OrderBy(e => e.Ordem))
+        {
+            Console.WriteLine(episodio.Resumo);
+        }
+        Console.WriteLine($"Esse podcast possui {TotalEpisodios} episódios");
+    }
+}
